bc1d66fcd9610f904b1c5cbef37c1d6968994600
Patrick Geers <patrick.geers@contentful.com>
2021-12-10 13:16:31 +0100

    Updated toggle buttons
    toggle button, close out, and field indicator update

--- src/components/EntryEditor.tsx
+++ src/components/EntryEditor.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Workbench, Typography, CheckboxField, Paragraph } from '@contentful/forma-36-react-components';
+import { Workbench, Typography, Paragraph, Select, Option, Button } from '@contentful/forma-36-react-components';
 import { EditorExtensionSDK } from '@contentful/app-sdk';
 import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
 
@@ -17,23 +17,40 @@ export default class EntryEditor extends React.Component <EditorProps, ConfigSta
     super(props)
    
     this.state = {
-      fields: props.sdk.entry.fields
+      fields: props.sdk.entry.fields,
     }
 
   } 
   async componentDidMount() {
     
+    // after the elements are on the page, go ahead and add the content
     try {
-      debugger
-      Object.keys(this.props.sdk.entry.fields).map((keyName, i) => (
-        document.getElementById('mydiv' + i + 'header')!.innerHTML = this.getFieldValue(this.props.sdk.entry.fields[keyName].id, i, 'mydiv' + i + 'header'),
+        Object.keys(this.props.sdk.entry.fields).map((keyName, i) => (
+        document.getElementById('mydiv' + i + 'header')!.innerHTML = this.getFieldValue(this.props.sdk.entry.fields[keyName].id, i),// 'mydiv' + i + 'header'),
         this.dragElement(document.getElementById('mydiv'+i.toString())!)
+ 
         ))
     }catch(err) {
       console.log(err)
     }
   }
 
+  // should probably be renamed to create or setContainerContent
+  async getEntry(entryID: string) {
+    //this.setState({selectedContentType: selectedContentType.value})
+     const entry = await this.props.sdk.space.getEntry(entryID)
+  }
+
+  setContainer = (keyName: string, i: number) => {
+    return (
+      <div className='container' id={'mydiv'+i.toString()} key={i}>
+      <div className='containerItem' id={'mydiv' + i + 'header'}></div>
+      <div className = 'fieldName'>{this.props.sdk.entry.fields[keyName].id} 
+      <div className='toggleElment' id={'mydiv' + i + 'toggl'} onClick={() => this.toggleElement(i)}>x</div></div>
+
+    </div>    
+  )
+  }
  
   dragElement = (elmnt: HTMLElement) => {    
     var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
@@ -78,37 +95,134 @@ export default class EntryEditor extends React.Component <EditorProps, ConfigSta
     }
   }
 
+  toggleElement = (el: Number) => {
+    debugger
+    var x = document.getElementById("mydiv"+el)
+    var y = document.getElementById("dot"+el)
+    if (x !== null && y !== null) {
+      if (x.style.display === "block") {
+        x.style.display = "none";
+        y.style.background = "red";
+      } else {
+        x.style.display = "block";
+        y.style.background = "#AAFF00";
 
-
-  getFieldValue = (field: string, i: number, div: String) => {
+      }
+    }
+  }
+  
+  getFieldValue = (field: string, i: number) => {
     
     let fieldValue = this.props.sdk.entry.fields[field].getValue();  
-    
      // let's not handle these now, we need a case later for all of the field types
     switch(this.props.sdk.entry.fields[field].type) {
-      case 'Symbol': { } break
-      case 'Text': {} break
-      case 'Integer': {} break
-      case 'Number': {} break
-      case 'Date': {
-        debugger
-        //fieldValue = new Date(fieldValue).toISOString().split('T')[0]
-      } break
-      case 'Location': {} break
-      case 'Boolean': { fieldValue === true ? fieldValue = 1 : fieldValue = 0 } break
-      case 'Link': {} break
-      case 'RichText': { fieldValue = documentToHtmlString(fieldValue) } break
-      case 'Object': { fieldValue = 'Object' } break 
-      case 'Array': { 
+      case 'Symbol':  
+        //nothing to do here since it is just text
+        //or check on widget type here?
+       break
+
+      case 'Text': 
+        //this needs markdown converter
+       break
+
+      case 'Integer': 
+        //nothing to do here since it is just integer
+       break
+
+      case 'Number': 
+        //nothing to do here since it is just integer
         
-        //this should be a list then? 
-        //if(typeof fieldValue !== undefined && this.props.sdk.entry.fields[field].items || 'Array') {
-        //  fieldValue = fieldValue.toString()
-        //}
-        fieldValue = 'Array'   
-      } break 
+       break
+
+      case 'Date': 
+        //convert the date to a more readable format
+        fieldValue = new Date(fieldValue).toISOString().split('T')[0]
+       break
+
+      case 'Location': 
+       // render the location (ideally in a map, but needs API key)
+      debugger;
+      let lat = fieldValue.lat
+      let long = fieldValue.lon
+       
+      fieldValue = '<img src="https://maps.google.com/maps/api/staticmap?center='+ lat + ','+ long +' '+ '&zoom=14&size=200x200&sensor=true&markers=|&key=AIzaSyDKWGl_R9LCIbyDfjd4HWAJXlA-i40HFLo" style="width: 400px; height: 400px;" />'
+
+       break
+
+      case 'Boolean': 
+        //converting bool to 1 or 0
+        //todo: other options? 
+        fieldValue === true ? fieldValue = 1 : fieldValue = 0 
+       break
+
+      case 'Link': 
+        
+      // need to resolve entry
+        if(fieldValue !== undefined) {
+          fieldValue = JSON.stringify(fieldValue)
+        }
+        //main work here for
+        // - images
+        // - single references
+        // - multiple references
+       break
+
+      case 'RichText':  
+        //convert the Rich Text to HTML
+        //update to handle inline entries
+        fieldValue = documentToHtmlString(fieldValue) 
+       break
+
+      case 'Object':  
+       
+       fieldValue = JSON.stringify(fieldValue)
+
+       break 
+
+      case 'Array': 
+      
+        // the type is Array, and we hav a link
+        if(Array.isArray(fieldValue) && this.props.sdk.entry.fields[field].items?.type === 'Link') { 
+          fieldValue.forEach(ref => {
+            //check for each link type
+            switch(ref.sys.linkType) {
+              
+              //Entry --> getEntry, then play through same scenario? 
+              case 'Entry':
+                
+                fieldValue = JSON.stringify(fieldValue)
+                //how do I call async here?
+                debugger
+                var entry =  this.getEntry(this.props.sdk.entry.fields[field].id)
+                
+                break
+              
+              //Asset --> getAsset, then show image
+              case 'Asset':
+                fieldValue = JSON.stringify(fieldValue)
+                break
+
+            }
+            //ref.sys.type
+          });
+        }
+
+        //case: for a list of items
+        if(fieldValue !== undefined && this.props.sdk.entry.fields[field].items && this.props.sdk.entry.fields[field].items?.type !== 'Link') {
+            
+          fieldValue = fieldValue.join(', ')
+
+        } else {
+          fieldValue = JSON.stringify(fieldValue)
+
+        }
+        
+        //fieldValue = 'Array'   
+     break
+    
       default: fieldValue = 'Not properly defined'
-    }
+    } 
+    //return the updated value to be rendered on the page
     return fieldValue
   }
 
@@ -117,28 +231,38 @@ export default class EntryEditor extends React.Component <EditorProps, ConfigSta
     return     (
       <Workbench>
         <Workbench.Sidebar position="left">
-          <Paragraph>Enable or disable content blocks via chekboxes.</Paragraph>
-          {
+          <Paragraph>Toggle field visibility</Paragraph>
           
+          {
             Object.keys(this.props.sdk.entry.fields).map((keyName, i) => (
-           <li id={'li'+this.props.sdk.entry.fields[keyName].id}>
-                <CheckboxField 
-                  name={this.props.sdk.entry.fields[keyName].id} 
-                  id={this.props.sdk.entry.fields[keyName].id} 
-                  labelText={this.props.sdk.entry.fields[keyName].id} labelIsLight />
-            </li>
-                
+              <li className="contentTypes" id={'li'+this.props.sdk.entry.fields[keyName].id} key={'li'+this.props.sdk.entry.fields[keyName].id}>
+
+                <Button className="btnToggle" size="small"
+                  onClick={() => this.toggleElement(i)}
+                   > {keyName} <span className="dot" id={'dot'+i}></span> </Button>
+              </li>
             ))
-            }
+          }
+            
+           
         </Workbench.Sidebar>
+        {/*<Workbench.Header
+          actions={
+            <Select name="optionSelect" id="optionSelect" width="medium">
+              <Option value="" disabled>
+                Select a predefined size...
+              </Option>
+              <Option value="optionOne">Mobile</Option>
+              <Option value="optionTwo">Tablet</Option>
+              <Option value="optionThree">Desktop</Option>
+            </Select>
+            }
+          />*/}
         <Workbench.Content>
           <Typography>
             {
             Object.keys(this.props.sdk.entry.fields).map((keyName, i) => (
-              <div className='container' id={'mydiv'+i.toString()} key={i}>
-                <div className='containerItem' id={'mydiv' + i + 'header'}></div>
-                <div className = 'fieldName'>{this.props.sdk.entry.fields[keyName].id} </div>
-              </div>            
+              this.setContainer(keyName, i) 
             ))
             }
           </Typography>
